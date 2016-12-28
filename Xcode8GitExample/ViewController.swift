d2b7617f62ca2c2eb138a733d125dfe9737bc02e
Smart Boy <smartboy@Smarts-MacBook-Pro.local>
2016-12-28 13:12:18 -0500

    test

--- Xcode8GitExample/ViewController.swift
+++ Xcode8GitExample/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
+        hh
         //additional comment
     }
 
